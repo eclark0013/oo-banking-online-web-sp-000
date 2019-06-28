2409b1ad470ef0b59acca235c1b7fffe5689f6d2
Eric Clark <eclark0013@gmail.com>
2019-06-28 18:19:32 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,9 +16,9 @@ class BankAccount
     "Your balance is $#{self.balance}."
   end
 
-  def valid?
-    if @status == "open" && @balance > 0 ? true : false
-  end
+  #def valid?
+  #  if @status == "open" && @balance > 0 ? true : false
+  #end
 
   def close_account
   end
